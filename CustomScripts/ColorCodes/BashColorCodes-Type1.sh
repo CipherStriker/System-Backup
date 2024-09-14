fa4abbd616ca3203ffb579f2bfa5e05f302293bb
CipherStriker <156460651+CipherStriker@users.noreply.github.com>
2024-09-14 20:20:57 +0530

    Add files via upload

---
+++ CustomScripts/ColorCodes/BashColorCodes-Type1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+# Color Codes : simply changes the text color
+# Foreground colors
+red='\e[31m'
+green='\e[32m'
+yellow='\e[33m'
+blue='\e[34m'
+magenta='\e[35m'
+cyan='\e[36m'
+white='\e[37m'
+
+# Background colors
+red_background='\e[41m'
+green_background='\e[42m'
+yellow_background='\e[43m'
+blue_background='\e[44m'
+magenta_background='\e[45m'
+cyan_background='\e[46m'
+white_background='\e[47m'
+
+# Bright foreground colors
+red_foreground='\e[91m'
+green_foreground='\e[92m'
+yellow_foreground='\e[93m'
+blue_foreground='\e[94m'
+magenta_foreground='\e[95m'
+cyan_foreground='\e[96m'
+white_foreground='\e[97m'
+
+# Bright Background colors
+red_bright_background='\e[101m'
+green_bright_background='\e[102m'
+yellow_bright_background='\e[103m'
+blue_bright_background='\e[104m'
+magenta_bright_background='\e[105m'
+cyan_bright_background='\e[106m'
+white_bright_background='\e[107m'
+
+# Text formatting
+bold='\e[1m'
+underlined='\e[4m'
+nc='\e[0m ' # No Color
---
+++ CustomScripts/ColorCodes/BashColorCodes-Type2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Color Codes : resets all text attributes and then changes the text color
+# Foreground colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+# Background colors
+RED_Background='\033[0;41m'
+GREEN_Background='\033[0;42m'
+YELLOW_Background='\033[0;43m'
+BLUE_Background='\033[0;44m'
+MAGENTA_Background='\033[0;45m'
+CYAN_Background='\033[0;46m'
+WHITE_Background='\033[0;47m'
+
+# Bright foreground colors
+RED_Foreground='\033[0;91m'
+GREEN_Foreground='\033[0;92m'
+YELLOW_Foreground='\033[0;93m'
+BLUE_Foreground='\033[0;94m'
+MAGENTA_Foreground='\033[0;95m'
+CYAN_Foreground='\033[0;96m'
+WHITE_Foreground='\033[0;97m'
+
+# Bright Background colors
+RED_Bright_Background='\033[0;101m'
+GREEN_Bright_Background='\033[0;102m'
+YELLOW_Bright_Background='\033[0;103m'
+BLUE_Bright_Background='\033[0;104m'
+MAGENTA_Bright_Background='\033[0;105m'
+CYAN_Bright_Background='\033[0;106m'
+WHITE_Bright_Background='\033[0;107m'
+
+# Text formatting
+bold='\033[0;1m'
+underlined='\033[0;4m'
+NC='\033[0;0m' # No Color
