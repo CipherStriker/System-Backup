2559aadd8fb0d8fa39d226b5856f20392ba4544e
CipherStriker <156460651+CipherStriker@users.noreply.github.com>
2024-12-02 12:33:02 +0530

    Delete yetToBeFinished directory

--- yetToBeFinished/extensive-recon.sh
+++
@@ -1,5 +0,0 @@
-#TODO
-#Recon with "directory-list-lowercase-2.3-medium.txt"
-#Recon with .jpg,.zip,.tar.gz
-#Scan file content with "raft-large-files-lowercase.txt"
-#Save the output file in the designated folder of the machine
